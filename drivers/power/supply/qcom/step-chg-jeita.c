0a5cb71116bb8948795398eed986be926a8c0912
deadman96385 <seanhoyt963@gmail.com>
2020-02-20 02:22:02 -0600

    Fix of_get_property can't get qcom,battery-data property

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -407,7 +407,7 @@ static int get_val(struct range_data *range, int hysteresis, int current_index,
 static int handle_step_chg_config(struct step_chg_info *chip)
 {
 	union power_supply_propval pval = {0, };
-	int rc = 0, fcc_ua = 0;
+	int rc = 0, fcc_ua = 0,ps_status;
 	u64 elapsed_us;
 
 	elapsed_us = ktime_us_delta(ktime_get(), chip->step_last_update_time);
@@ -441,6 +441,23 @@ static int handle_step_chg_config(struct step_chg_info *chip)
 			pval.intval,
 			&chip->step_index,
 			&fcc_ua);
+	// QCI Rex, modify for battery spec. - S
+	// ugly code,
+	// if step charge current and jeita current both equal 0.8A,
+	// it means v in 4.2~4.4 and t in 10~23.
+	// In this condition, fcc_ua should be 0.4A
+	if (800000 == fcc_ua && 800000 == jeita_fcc_ua) {
+		fcc_ua = 400000;
+	}
+	// QCI Rex, modify for battery spec. - E
+
+	// QCI Rex, when don, we limit charging current to 0.2A
+	ps_status = si1142ps_query_status();
+	if (0 == ps_status && fcc_ua > 400000) {
+                fcc_ua = 200000;
+                pr_info("limiting charging current of donned device, "
+                        "fcc_ua=%d uA", fcc_ua);
+        }
 
 	if (rc < 0) {
 		/* remove the vote if no step-based fcc is found */
@@ -471,7 +488,7 @@ reschedule:
 static int handle_jeita(struct step_chg_info *chip)
 {
 	union power_supply_propval pval = {0, };
-	int rc = 0, fcc_ua = 0, fv_uv = 0;
+	int rc = 0, fcc_ua = 0, fv_uv = 0, ps_status;
 	u64 elapsed_us;
 
 	rc = power_supply_get_property(chip->batt_psy,
